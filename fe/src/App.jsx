e0be460889f72042dbd68a673fcc1156cb47f7f6
ShantanuGaur26 <gaurshantanugaur@gmail.com>
2025-03-13 02:23:43 +0530

    huh

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <button
         onClick={async () => {
           try {
-            const response = await axios.get("https://deploy-be-dusky.vercel.app/");
+            const response = await axios.get("https://deploy-be-dusky.vercel.app");
             setOutput(response.data); // Assuming the response contains a string or valid data
           } catch (error) {
             console.error("Error fetching data:", error);
