9f774b5dfc2cc40a7a2df4e6ec2f5f23217e68dd
ShantanuGaur26 <gaurshantanugaur@gmail.com>
2025-03-12 23:09:11 +0530

    init

---
+++ fe/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button onClick={()=>{
+      }}>click</button>
+    </>
+  )
+}
+
+export default App
